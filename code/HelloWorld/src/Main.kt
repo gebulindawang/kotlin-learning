78eef212ca4ee454bf0dfa17cb984c5b9aa883fb
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-11-28 11:10:56 +0800

    对象的创建与使用

--- code/HelloWorld/src/Main.kt
+++ code/HelloWorld/src/Main.kt
@@ -2,5 +2,9 @@ import javax.annotation.processing.Messager
 import javax.print.DocFlavor
 
 fun main() {
+    val p1:Student = Student("张三", 18)
+    val p2:Student = p1
+    p2.name = "李四"
+    println(p1.name)
 }
 
---
+++ code/HelloWorld/src/Student.kt
@@ -0,0 +1,7 @@
+class Student constructor (var name: String,var age: Int){
+
+    fun sayHello(): String {
+        println("Hello, my name is $name, and I am $age years old.")
+        return "Hello, my name is $name, and I am $age years old."
+    }
+}
