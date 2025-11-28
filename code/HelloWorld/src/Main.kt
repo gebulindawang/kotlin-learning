ebd0fe538b1b08ff9c4d4669e5e5b57d1bc6b3c2
超级无敌哥布林大王 <15176388+super-invincible-goblin-king@user.noreply.gitee.com>
2025-11-28 10:39:50 +0800

    内联函数

--- code/HelloWorld/src/Main.kt
+++ code/HelloWorld/src/Main.kt
@@ -2,14 +2,5 @@ import javax.annotation.processing.Messager
 import javax.print.DocFlavor
 
 fun main() {
-    val func:(Int) -> String = func@{
-        if (it > 10){
-            return@func "我是提前返回值"
-        }
-        "我是默认返回值"
-    }
-    print(func(11))
 }
-fun test(func:(String) -> Int){
-    println(func("hello"))
-}
+
